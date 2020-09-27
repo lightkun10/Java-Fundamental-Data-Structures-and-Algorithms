2c4efd6e0d8e4c2950b3f0bef93be18414522ddb
lightkun10 <lifeisubw@gmail.com>
2020-09-28 05:57:33 +0700

    Add chapter 20.2 Collections

--- Book/Reading/C20_Lists_Stacks_Queues_and_Priority_Queues/C20_2_Collections/TestCollection.java
+++ Book/Reading/C20_Lists_Stacks_Queues_and_Priority_Queues/C20_2_Collections/TestCollection.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class TestCollection {
     public static void main(String[] args) {
+        // The program creates a concrete collection object using ArrayList
         ArrayList<String> collection1 = new ArrayList<>();
 
         collection1.add("New York");
@@ -14,10 +15,14 @@ public class TestCollection {
         System.out.println("A list of cities in collection1: ");
         System.out.println(collection1);
 
+        // Arraylist invokes the Collection interface’s contains method
         System.out.println("\nIs Dallas in collection1? "   // contains?
             + collection1.contains("Dallas"));
 
+        // Arraylist invokes the Collection interface’s remove method
         collection1.remove("Dallas");
+
+        // Arraylist invokes the Collection interface’s size method
         System.out.println("\n" + collection1.size() +
             " cities are in collection1 now");
         
@@ -30,19 +35,36 @@ public class TestCollection {
         System.out.println("\nA list of cities in collection2:");
         System.out.println(collection2);
 
+        // The program creates a copy of an array list
         ArrayList<String> c1 = (ArrayList<String>)(collection1.clone()); // clone
+
+        // Arraylist invokes the Collection interface’s addAll method
+        // Add all the elements from one collection to another collection
         c1.addAll(collection2);     // addAll
+
         System.out.println("\nCities in collection1 or collection2: ");
         System.out.println(c1);
 
+        // The program creates a copy of an array list
         c1 = (ArrayList<String>)(collection1.clone());
+
+        // Arraylist invokes the Collection interface’s retainAll method
+        // The retainAll method retains the elements in this
+        // collection that are also present in the specified collection
         c1.retainAll(collection2);      // retainAll
         System.out.print("\nCities in collection 1 and collection2:");
         System.out.println(c1);
         
+        // The program creates a copy of an array list
         c1 = (ArrayList<String>)(collection1.clone());
+
+        // Arraylist invokes the Collection interface’s removeAll method
+        // The removeAll method removes the elements from this 
+        // collection that are present in the specified collection
         c1.removeAll(collection2);
         System.out.print("\nCities in collection1, but not in 2: ");
         System.out.println(c1);
+
+        
     }
 }
